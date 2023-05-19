97c464ad517b6912e7959f8af5f5bf3512f786ca
hawkf07 <hawkf07@proton.me>
2023-05-19 19:34:05 +0700

    feat:design initial look

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,30 +5,23 @@ type Navbar = {
 
 export const Navbar = () => {
   return (
-    <nav className="flex justify-around bg-blue-500 p-3 text-white ">
-      <header>
-        <h1 className="font-bold uppercase">
-          <a href="#">Logo</a>
-        </h1>
-      </header>
-      <ul className="hidden w-1/3  items-center justify-around md:flex">
-        <li>
-          <a href="#">Link 1</a>
-        </li>
-        <li>
-          <a href="#">Link 2</a>
-        </li>
-        <li>
-          <a href="#">Link 3</a>
-        </li>
-        <li>
-          <a href="#">Link 4</a>
-        </li>
-      </ul>
-
-      <button className="block md:hidden">
-        <FaBars />
-      </button>
-    </nav>
+    <header className="flex justify-around border p-3 ">
+      <h1 className="font-bold uppercase">
+        <a href="#">FiReddit</a>
+      </h1>
+      <nav>
+        <ul className="hidden items-center  justify-around gap-8 md:flex">
+          <li>
+            <a href="#">About</a>
+          </li>
+          <li>
+            <a href="#">SignUp</a>
+          </li>
+        </ul>
+        <button className="block md:hidden">
+          <FaBars />
+        </button>
+      </nav>
+    </header>
   );
 };
